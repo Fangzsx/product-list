78a36f97adee13bf896a8078bea7c5c6cdb99c6d
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-28 11:54:08 +0800

    add product list

---
+++ app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/product-list.json`)
+);
+
+//show all products
+app.get('/api/v1/products', (req, res) => {
+    res.status(200).json({
+        status : 'success',
+        data : {
+            products : products
+        }
+    })
+
+})
+
+const port = 3000;
+app.listen(3000, () => {
+    console.log(`app started with port ${port}`);
+});
