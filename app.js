c626601308716b2e3a0a7f9fe8b0846a1c599b30
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-28 12:12:13 +0800

    todo : post a new product

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const products = JSON.parse(
     String(fs.readFileSync(`${__dirname}/dev-data/data/product-list.json`))
 );
@@ -10,11 +12,17 @@ const products = JSON.parse(
 app.get('/api/v1/products', (req, res) => {
     res.status(200).json({
         status : 'success',
+        results : products.length,
         data : {
             products : products
         }
     })
 
+})
+
+app.post('/api/v1/products', (req, res) => {
+    console.log(req.body)
+    res.send('done');
 })
 
 const port = 3000;
