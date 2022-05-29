ce65ba31c9bc2b066597c593392257cabbb85492
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-29 08:25:57 +0800

    refactor routes

--- app.js
+++ app.js
@@ -8,39 +8,7 @@ const products = JSON.parse(
     String(fs.readFileSync(`${__dirname}/dev-data/data/product-list.json`))
 );
 
-//show all products
-app.get('/api/v1/products', (req, res) => {
-    res.status(200).json({
-        status : 'success',
-        results : products.length,
-        data : {
-            products : products
-        }
-    })
-
-})
-
-app.get('/api/v1/products/:id', (req, res) => {
-    const id = req.params.id * 1;
-    const product = products.find(product => product.id === id)
-
-    if(!product){
-        return res.status(404).json({
-            status : 'fail',
-            message : 'Invalid id'
-        });
-    }
-
-    res.status(200).json({
-       status : 'success',
-       data : {
-           product
-       }
-    });
-
-});
-
-app.patch('/api/v1/products/:id', (req, res) => {
+const updateProduct = (req, res) => {
     const id = req.params.id * 1
 
     const product = products.find((product) => product.id === id)
@@ -56,11 +24,52 @@ app.patch('/api/v1/products/:id', (req, res) => {
         data : {
             product : '<Updated product here>'
         }
+    });
+
+};
+const getAllProducts = (req, res) => {
+    res.status(200).json({
+        status : 'success',
+        results : products.length,
+        data : {
+            products : products
+        }
     })
+};
+const getProduct = (req, res) => {
+    const id = req.params.id * 1;
+    const product = products.find(product => product.id === id)
 
-})
+    if(!product){
+        return res.status(404).json({
+            status : 'fail',
+            message : 'Invalid id'
+        });
+    }
 
-app.post('/api/v1/products', (req, res) => {
+    res.status(200).json({
+        status : 'success',
+        data : {
+            product
+        }
+    });
+};
+const deleteProduct = (req, res) => {
+    const id = req.params.id * 1;
+    const product = products.find((product) => product.id === id)
+    if(!product){
+        res.status(404).json({
+            status : 'fail',
+            message : 'Invalid id'
+        });
+    }
+
+    res.status(204).json({
+        status : 'success',
+        data : null
+    });
+};
+const createProduct = (req, res) => {
     const id = products[products.length-1].id + 1;
     const newProduct = Object.assign({id : id}, req.body);
     products.push(newProduct);
@@ -71,9 +80,27 @@ app.post('/api/v1/products', (req, res) => {
             data: {
                 product : newProduct
             }
-        })
-    })
-})
+        });
+    });
+};
+
+app.route('/api/v1/products')
+    .get(getAllProducts)
+    .post(createProduct);
+
+app.route('/api/v1/products/:id')
+    .patch(updateProduct)
+    .delete(deleteProduct)
+    .get(getProduct)
+
+//show all products
+/*
+app.get('/api/v1/products', getAllProducts);
+app.get('/api/v1/products/:id', getProduct);
+app.patch('/api/v1/products/:id', updateProduct);
+app.delete('/api/v1/products/:id', deleteProduct);
+app.post('/api/v1/products', createProduct);
+*/
 
 const port = 3000;
 app.listen(3000, () => {
