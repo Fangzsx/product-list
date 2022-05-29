85156634918cedefe074bfa5c3ac8453e32c79d5
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-05-29 11:11:17 +0800

    set limit for user id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,17 @@
+exports.checkID = (req, res, next, val) => {
+    const id = req.params.id
+
+
+    if(id > 5) {
+        console.log(`params id ${id}`);
+        return res.status(404).json({
+            status : 'fail',
+            message :'Invalid, max id param 4'
+        })
+    }
+    next();
+}
+
 exports.createUser = (req, res) => {
     res.status(500).json({
         status : 'error',
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userController = require('./../controllers/userController');
 
+router.param('id', userController.checkID);
+
 
 router.route('/')
     .get(userController.getAllUsers)
